0fa20394f3f52a7c5dad27849e5b766717278833
Chris Yeganeh <christianyeganeh@gmail.com>
2025-03-25 03:09:21 +0000

    solved p7

--- lua/src/p7.lua
+++ lua/src/p7.lua
@@ -1,6 +1,11 @@
+local utils = require("src/utils")
+
 local function main()
-	-- TODO
-	return 0
+	local p_iter = utils.prime.prime_iter()
+	for i=1,10000 do
+		p_iter()
+	end	
+	return p_iter()
 end
 return {
 	problem = main,
